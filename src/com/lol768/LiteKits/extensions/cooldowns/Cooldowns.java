168ceae37db0d586e82f47220a618c5feb6747c1
lol768 <support@technostripe.com>
2013-05-19 21:29:16 +0100

    Unused imports

--- src/com/lol768/LiteKits/extensions/cooldowns/Cooldowns.java
+++ src/com/lol768/LiteKits/extensions/cooldowns/Cooldowns.java
@@ -7,14 +7,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.lol768.LiteKits.LiteKits;
 import com.lol768.LiteKits.API.KitCheckEvent;
-import com.lol768.LiteKits.utility.Messaging;
 
 public class Cooldowns extends JavaPlugin implements Listener {
     private LiteKits lk;
